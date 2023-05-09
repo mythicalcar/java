4d3cd0a87226c94023deee506aa9197fd0f90319
Adamski07 <s1178952@student.windesheim.nl>
2023-05-09 12:54:32 +0200

    sui

--- scr/Database.java
+++ scr/Database.java
@@ -1,17 +1,30 @@
 package scr;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class Database {
-    String url = "jdbc:mysql://localhost:3306/nerdygadgets";
+
     public static void main(String[] args) {
         try {
+            String url = "jdbc:mysql://localhost:3306/nerdygadgets";
+            String username = "root";
+            String password = "";
+
             Class.forName("com.mysql.cj.jbdc.Driver");
 
-            Connection connection = DriverManager.getConnection(url);
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            ResultSet city = statement.executeQuery("select CityID, CityName from cities;");
+            
+            while (city.next()){
+                System.out.println(city.getInt(1) + " " + city.getString(2));
+            }
+
+            connection.close();
         } catch (ClassNotFoundException e) {
             System.out.println("e");
+        } catch (SQLException e) {
+            System.out.println("suii");;
         }
     }
 }
