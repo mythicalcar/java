e7690235723386ae2d67c66f4672f79e88a8a450
Adamski07 <s1178952@student.windesheim.nl>
2023-05-16 23:49:53 +0200

    afstanden

---
+++ scr/Distances.java
@@ -0,0 +1,47 @@
+package scr;
+
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.DistanceMatrixApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.*;
+
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.DistanceMatrixApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DistanceMatrix;
+
+public class Distances {
+    private String apiKey = "AIzaSyDLqeDXpQB6vLjO5sh7oJrp0twa8qyJXIE";
+
+
+
+    public double calculateDistance(String sourcePlace, String destinationPlace) {
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey(apiKey)
+                .build();
+
+        try {
+            DistanceMatrixApiRequest request = DistanceMatrixApi.newRequest(context)
+                    .origins(sourcePlace)
+                    .destinations(destinationPlace);
+
+            DistanceMatrix matrix = request.await();
+
+            DistanceMatrixRow row = matrix.rows[0];
+            DistanceMatrixElement element = row.elements[0];
+
+            if (element.status == DistanceMatrixElementStatus.OK) {
+                int distanceInMeters = (int) element.distance.inMeters;
+                double distanceInKilometers = distanceInMeters / 1000.0;
+
+                return distanceInKilometers;
+            } else {
+                System.out.println("Unable to calculate distance. Status: " + element.status);
+                return -1; // Return a negative value to indicate an error
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1; // Return a negative value to indicate an error
+        }
+    }
+}
--- scr/Main.java
+++ scr/Main.java
@@ -3,12 +3,31 @@ package scr;
 import com.mysql.cj.xdevapi.AbstractDataResult;
 
 import javax.xml.crypto.Data;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Database db1 = new Database();
-        db1.selectNames(1);
-        Adress f1 = new Adress("Adam", "Abdelbakey", "rgr", "1056DN");
+//    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//        Database db1 = new Database();
+////        db1.selectNames(1);
+//        Adress f1 = new Adress("Adam", "Abdelbakey", "rgr", "1056DN");
+//
+//    }
+
+    public static void main(String[] args) {
+        String apiKey = "YOUR_API_KEY";
+        Distances distanceCalculator = new Distances();
+
+        String sourcePlace = "Marco Polostraat 291A";
+        String destinationPlace = "Almere";
+
+        double distance = distanceCalculator.calculateDistance(sourcePlace, destinationPlace);
+
+        if (distance >= 0) {
+            System.out.println("Distance: " + distance + " km");
+        } else {
+            System.out.println("An error occurred while calculating the distance.");
+        }
     }
 }
+
