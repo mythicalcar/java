c21d868909834b83b16d8cdf9b8350bf3bff008a
jeff <s1170385@student.windesheim.nl>
2023-05-26 20:13:27 +0200

    bruh

--- src/BezorgerMenuPage.java
+++ src/BezorgerMenuPage.java
@@ -4,20 +4,48 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class BezorgerMenuPage extends JPanel implements ActionListener {
     private JButton backButton;
     private ApplicationFrame applicationFrame;
+    private JPanel gridBagPanel;
+    private JLabel menuTitleLabel;
+    private JTable bezorgerJTable;
+    private JScrollPane bezorgerScrollPane;
+    private String[] columnNames = {
+            "Naam",
+            "idk",
+    };
     public BezorgerMenuPage(ApplicationFrame applicationFrame){
         this.applicationFrame = applicationFrame;
-        setLayout(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
+        setLayout(new BorderLayout());
+
+        //make components, add listeners and constraints
+        menuTitleLabel = new JLabel("Bezorgers beheren");
+
+        gridBagPanel = new JPanel(new GridBagLayout());
+
+        bezorgerJTable = new JTable();
+
+        bezorgerScrollPane = new JScrollPane();
 
         backButton = new JButton("Terug");
         backButton.addActionListener(this);
-        constraints.gridx = 2;
-        constraints.gridy = 2;
-        add(backButton);
+        GridBagConstraints backButtonc = new GridBagConstraints();
+        backButtonc.gridx = 0;
+        backButtonc.gridy = 3;
+        backButtonc.insets = new Insets(0, 50, 50, 0);
+        backButtonc.anchor = GridBagConstraints.LAST_LINE_START;
+
+
+
+        //add components to gridbagpanel
+        gridBagPanel.add(backButton);
+
+        //add components to bezorgermenupage
+        this.add(menuTitleLabel, BorderLayout.PAGE_START);
+        this.add(gridBagPanel, BorderLayout.CENTER);
     }
 
     @Override
@@ -25,5 +53,10 @@ public class BezorgerMenuPage extends JPanel implements ActionListener {
         if(e.getSource() == backButton){
             setVisible(false);
         }
+    }
+
+    private ArrayList<String> getBezorgers(){
+        ArrayList<String> bezorgers = new ArrayList<String>();
+        return bezorgers;
     }
 }
--- src/ManagerPage.java
+++ src/ManagerPage.java
@@ -1,6 +1,7 @@
 package src;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,42 +10,63 @@ public class ManagerPage extends JPanel implements ActionListener {
     private JButton bezorgerMenuButton;
     private JButton routeMenuButton;
     private ApplicationFrame applicationFrame;
+    private JPanel gridBagPanel;
+    private JLabel welcomeLabel;
+    private JLabel emptyBottomLabel;
     public ManagerPage(ApplicationFrame applicationFrame){
         this.applicationFrame = applicationFrame;
-        setLayout(new GridBagLayout());
+        setLayout(new BorderLayout());
 
-        GridBagConstraints constraints;
+        //make components, bind actions and define constraints if needed
+        gridBagPanel = new JPanel(new GridBagLayout());
+        gridBagPanel.setAlignmentX(JPanel.CENTER_ALIGNMENT);
 
-        JLabel welcomeLabel = new JLabel("Welkom " + "manager!");
-        constraints = new GridBagConstraints();
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.gridwidth = 4;
-        constraints.gridheight = 1;
-        //constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.anchor = GridBagConstraints.PAGE_START;
-        constraints.insets = new Insets(0, 0, 150, 0);
-        add(welcomeLabel, constraints);
+        welcomeLabel = new JLabel("Welkom " + "manager!");
+        welcomeLabel.setHorizontalAlignment(JLabel.CENTER);
+        welcomeLabel.setPreferredSize(new Dimension(200, 100));
+
+        emptyBottomLabel = new JLabel();
+        emptyBottomLabel.setPreferredSize(new Dimension(200, 100));
 
         bezorgerMenuButton = new JButton("Bezorgers beheren");
         bezorgerMenuButton.addActionListener(this);
-        constraints = new GridBagConstraints();
-        constraints.gridx = 1;
-        constraints.gridy = 2;
-        constraints.gridwidth = 2;
-        constraints.gridheight = 1;
-        add(bezorgerMenuButton, constraints);
-
+        GridBagConstraints bezorgerMenuButtonc = new GridBagConstraints();
+        bezorgerMenuButtonc.gridx = 1;
+        bezorgerMenuButtonc.gridy = 1;
+        bezorgerMenuButtonc.gridwidth = 2;
+        bezorgerMenuButtonc.gridheight = 1;
+        bezorgerMenuButtonc.fill = GridBagConstraints.HORIZONTAL;
         routeMenuButton = new JButton("Routes beheren");
         routeMenuButton.addActionListener(this);
-        constraints = new GridBagConstraints();
-        constraints.gridx = 1;
-        constraints.gridy = 3;
-        constraints.gridwidth = 2;
-        constraints.gridheight = 1;
-        constraints.fill = GridBagConstraints.HORIZONTAL;
+        GridBagConstraints routeMenuButtonc = new GridBagConstraints();
+        routeMenuButtonc.gridx = 1;
+        routeMenuButtonc.gridy = 2;
+        routeMenuButtonc.gridwidth = 2;
+        routeMenuButtonc.gridheight = 1;
+        routeMenuButtonc.fill = GridBagConstraints.HORIZONTAL;
 
-        add(routeMenuButton, constraints);
+        //add components to gridbagpanel
+        gridBagPanel.add(bezorgerMenuButton, bezorgerMenuButtonc);
+        gridBagPanel.add(routeMenuButton, routeMenuButtonc);
+        //add components to applicationframe
+        this.add(welcomeLabel, BorderLayout.PAGE_START);
+        this.add(gridBagPanel, BorderLayout.CENTER);
+        this.add(emptyBottomLabel, BorderLayout.PAGE_END);
+
+
+//        constraints = new GridBagConstraints();
+//        constraints.gridx = 0;
+//        constraints.gridy = 0;
+//        constraints.gridwidth = 4;
+//        constraints.gridheight = 1;
+        //constraints.fill = GridBagConstraints.HORIZONTAL;
+//        constraints.anchor = GridBagConstraints.PAGE_START;
+//        constraints.insets = new Insets(0, 0, 150, 0);
+//        add(welcomeLabel, constraints);
+
+
+
+
     }
 
     @Override
